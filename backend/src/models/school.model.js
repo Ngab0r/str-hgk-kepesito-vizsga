659c1aa60f879ea6fe933c910585a30183355fe7
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-09-06 13:34:32 +0200

    2. feladat

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,27 @@
 /**
  * Exercise: make School mongoose model
  */
+const mongoose = require('mongoose');
+
+const School = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    classrooms: {
+        type: [string],
+        required: false
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = new mongoose.model('School', SchoolSchema, 'school');
